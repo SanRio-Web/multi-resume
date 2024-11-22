1d79fb7a30ccecf80baf81b82cac229403dd6eff
SanRio-Web <sanrioDev@gmail.com>
2024-11-22 02:31:16 +0200

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+  return (
+    <>
+      <div className="content">
+        <h1>Future Projects!</h1>
+        <p>Projects featured today by our curators</p>
+
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+
+const Navigation = () => {
+  return (
+    <>
+      <nav className="navigation">
+        <div className="navigation__box">
+          Будущая реклама!  {/* <img  src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNjOWczZnJzNm81NnJ6aXNhNDcyeGc2cTJtdmM3N2JxZXh5eTJqdyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/o75ajIFH0QnQC3nCeD/giphy.gif' /> */}
+        </div>
+        <div className="navigation__box">
+          Будущая реклама!  {/* <img  src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNjOWczZnJzNm81NnJ6aXNhNDcyeGc2cTJtdmM3N2JxZXh5eTJqdyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/o75ajIFH0QnQC3nCeD/giphy.gif' /> */}
+        </div>
+        <div className="navigation__box">
+          Будущая реклама!  {/* <img  src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNjOWczZnJzNm81NnJ6aXNhNDcyeGc2cTJtdmM3N2JxZXh5eTJqdyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/o75ajIFH0QnQC3nCeD/giphy.gif' /> */}
+        </div>
+        <div className="navigation__box">
+          Будущая реклама!  {/* <img  src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWNjOWczZnJzNm81NnJ6aXNhNDcyeGc2cTJtdmM3N2JxZXh5eTJqdyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/o75ajIFH0QnQC3nCeD/giphy.gif' /> */}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
